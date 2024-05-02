21e1edb14c4e6e5ce8c7476f80ae12526c761850
KateZavadskaya <ekm.zavadskaya@gmail.com>
2024-05-02 14:08:41 +0300

    Fix(linter): fix linter error C0114 in main.py
    Add docstring ''' 'main' function...console.''' at the top
    
    main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+""" 'main' function prints the string 'Hello world!' to the console."""
+
+
 def main():
     """ 'main' function prints  the string 'Hello world!' to the console."""
     print("Hello world!")
